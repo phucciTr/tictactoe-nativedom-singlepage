e59e790771fec99b2bca5b45bda69f4b488bdfd9
phucciTr <miabam26@gmail.com>
2021-01-27 22:20:15 -0800

    Refactored for MVC

--- app.js
+++ app.js
@@ -1,11 +1,6 @@
-console.log('hello');
+let table, nonTakenSquares, humanTurn, machineTurn, gameOver;
 
-let table = {};
-let nonTakenSquares = {};
-let humanTurn = true;
-let machineTurn = false;
-let gameOver = false;
-
+// Getters / setters
 var getContainer = () => document.getElementById('container');
 var getSquares = () => Array.from(document.getElementsByTagName('td'));
 var getWinText = () => document.getElementById('winText');
@@ -14,46 +9,136 @@ var enableSquare = (square) => square.style['pointer-events'] = 'auto';
 var getId = (square) => square.id.split('sqr')[1];
 var getPlayer = (player) => player.innerText;
 
+var setUpTable = () => {
+  table = {};
+  nonTakenSquares = {};
+  humanTurn = true;
+  machineTurn = false;
+  gameOver = false;
+};
 
-var disableClick = () => document.getElementById('matrix').style['pointer-events'] = 'none';
-var enableHumanTurn = () => document.getElementById('matrix').style['pointer-events'] = 'auto';
+var disableClick = (cb) => document.getElementById('matrix').style['pointer-events'] = 'none';
+var enableClick = () => document.getElementById('matrix').style['pointer-events'] = 'auto';
 var currentTurn = () => humanTurn ? 'human' : 'machine';
 var refreshButton = () => document.getElementById('refresh');
 var isTight = () => Object.keys(nonTakenSquares).length === 0;
+var startGame = () => initializeTable();
+
+var resetGame = () => {
+  getSquares().forEach((square) => {
+      square.innerText = '';
+      enableSquare(square);
+      square.removeAttribute('style');
+  });
+
+  setUpTable();
+  getWinText().remove();
+  enableClick();
+  initializeTable();
+};
+
+// Set up table and event listeners
+var initializeTable = () => {
+  setUpTable();
+
+  getSquares().forEach((square) => {
+    square.addEventListener(('click'), clickHandler);
+    let id = getId(square);
+    table[id] = null;
+    nonTakenSquares[id] = id;
+  });
+  refreshButton().addEventListener(('click'), (e) => {
+    if (gameOver) { resetGame(); }
+  });
+
+};
+
+// Controller
+var clickHandler = (e) => handleClick(e);
 
 
+// Model
+var handleClick = (e) => {
+  placeMove(e.target);
 
-var switchTurn = (clickedId) => {
+  let clickedId = getId(e.target);
+  let player = getPlayer(e.target);
 
+  disableSquare(e.target);
+  updateTable(clickedId, player);
+  checkWinner(player);
+};
+
+// View
+var placeMove = (square) => square.innerText = 'X';
+
+
+// Start
+startGame();
+
+
+// Helpers
+var updateTable = (id, player) => {
+  table[id] = player;
+  delete nonTakenSquares[id];
+};
+
+var checkWinner = (player) => {
+  if (hasDiagWin(player) || hasColWin(player) || hasRowWin(player)) {
+    gameOver = true;
+    showWinner(player);
+    disableClick();
+
+  } else if (isTight()) {
+    gameOver = true;
+    showWinner();
+    disableClick();
+
+  } else { switchTurn(); }
+};
+
+var hasDiagWin = (player) => checkWinDirection(player, 'diag');
+var hasColWin = (player) => checkWinDirection(player, 'col');
+var hasRowWin = (player) => checkWinDirection(player, 'row');
+
+
+var checkWinDirection = (player, direction) => {
+  return getPossibleWins(direction, (poss1, poss2, poss3) => {
+    return hasWin(player, poss1, poss2, poss3);
+  });
+};
+
+var switchTurn = () => {
   if (currentTurn() === 'human') {
     humanTurn = false;
     machineTurn = true;
     disableClick();
-    generateMove(clickedId);
+    generateMove();
 
   } else if (currentTurn() === 'machine') {
     humanTurn = true;
     machineTurn = false;
-    enableHumanTurn();
+    enableClick();
   }
 }
 
-var generateMove = (clickedId) => {
+var generateMove = (cb) => {
   let randomInt = Math.floor(Math.random() * (Object.keys(nonTakenSquares).length - 1) + 0);
   let squareId = Object.values(nonTakenSquares)[randomInt];
   let square = document.getElementById(`sqr${squareId}`);
-  square.innerText = 'O';
 
-  disableSquare(square);
-  updateTable(squareId, 'O');
-  checkWinner('O');
+  setTimeout(() => {
+    square.innerText = 'O';
+    disableSquare(square);
+    updateTable(squareId, 'O');
+    checkWinner('O');
+  }, 1000);
 };
 
 
 var showWinner = (player) => {
   let winMessage = player ? getWinMessage(player) : getWinMessage();
   getContainer().append(winMessage);
-  disableClick();
 };
 
 var getWinMessage = (player) => {
@@ -67,36 +152,6 @@ var getWinMessage = (player) => {
   return winMessage;
 };
 
-
-var checkWinner = (player) => {
-
-  if (hasDiagWin(player) || hasColWin(player) || hasRowWin(player)) {
-    gameOver = true;
-    showWinner(player);
-    disableClick();
-
-  } else if (isTight()) {
-    gameOver = true;
-    showWinner();
-    disableClick();
-
-  } else { switchTurn(); }
-
-};
-
-
-
-
-var hasDiagWin = (player) => checkWinDirection(player, 'diag');
-var hasColWin = (player) => checkWinDirection(player, 'col');
-var hasRowWin = (player) => checkWinDirection(player, 'row');
-
-
-var checkWinDirection = (player, direction) => {
-  return getPossibleWins(direction, (poss1, poss2, poss3) => {
-    return hasWin(player, poss1, poss2, poss3);
-  });
-};
 
 var getPossibleWins = (dir, cb) => {
   let possibility1, possibility2, possibility3;
@@ -132,57 +187,11 @@ var hasWin = (player, poss1, poss2, poss3) => {
 };
 
 
-var placeMove = (square) => square.innerText = 'X';
 
 
-var clickHandler = (e) => {
 
-  placeMove(e.target);
-  disableSquare(e.target);
 
-  let clickedId = getId(e.target);
-  let player = getPlayer(e.target);
 
-  updateTable(clickedId, player);
-  checkWinner(player);
-}
 
-var updateTable = (id, player, callback) => {
-  table[id] = player;
-  delete nonTakenSquares[id];
-};
-
-var initializeTable = () => {
-  getSquares().forEach((square) => {
-    square.addEventListener(('click'), clickHandler);
-    let id = getId(square);
-    table[id] = null;
-    nonTakenSquares[id] = id;
-  });
-  refreshButton().addEventListener(('click'), (e) => {
-    if (gameOver) { resetGame(); }
-  });
-
-};
-
-var resetGame = () => {
-  getSquares().forEach((square) => {
-      square.innerText = '';
-      enableSquare(square);
-      square.removeAttribute('style');
-  });
-  table = {};
-  nonTakenSquares = {};
-  gameOver = false;
-  humanTurn = true;
-  machineTurn = false;
-  getWinText().remove();
-  enableHumanTurn();
-  initializeTable();
-};
-
-var startGame = () => initializeTable();
-
-startGame();
 
 
